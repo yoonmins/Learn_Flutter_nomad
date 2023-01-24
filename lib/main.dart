6493c5ebfe2a93de6ec436dd551064e39ded3176
ysmgg1@naver.com <yoonmin.tech@gmail.com>
2023-01-24 17:34:44 +0900

    Counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,63 @@
-import 'package:apptoon/widgets/button.dart';
-import 'package:apptoon/widgets/currency_card.dart';
+//import 'package:apptoon/widgets/button.dart';
+//import 'package:apptoon/widgets/currency_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState((){
+    counter = counter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                  iconSize: 40,
+                  onPressed: onClicked,
+                  icon: const Icon(Icons.add_box),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//wallet app
+/*
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -144,3 +196,4 @@ class App extends StatelessWidget {
     );
   }
 }
+ */
